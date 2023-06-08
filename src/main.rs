79e621099e7d2714a3a93e6a23c31f6b3bc2c375
Jack Sipley <sipleyjack@gmail.com>
2023-06-08 15:49:19 -0400

    Added task data xml file for testing, read_xml function signature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::BufReader;
+use xml::reader::{EventReader, XmlEvent};
+
+struct Task {
+    description: String,
+    due_date: String,
+    important: String,
+}
+
+fn read_xml(filename: &str) -> Result<Vec<Task>, Box<dyn std::error::Error>> {
+    let mut task_list: Vec<Task> = Vec::new();
+    Ok(task_list)
+}
+
 fn main() {
     println!("Hello, world!");
 }
