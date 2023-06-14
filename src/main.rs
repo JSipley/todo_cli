86f727590a8df56da8dbdf6eed436c6e1c5b1181
Jsipley <sipleyjack@gmail.com>
2023-06-13 22:19:02 -0400

    implemented method for handling tasks

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,34 @@ use std::fs::File;
 use std::io::BufReader;
 use xml::reader::{EventReader, XmlEvent};
 
+#[derive(Debug)]
 struct Task {
     description: String,
     due_date: String,
     important: String,
 }
+
+struct TaskManager {
+    tasks: Vec<Task>,
+}
+
+impl TaskManager {
+    fn set_tasks(&mut self, new_tasks: Vec<Task>) {
+        self.tasks = new_tasks;
+    }
+
+    fn fetch_tasks(&self) -> &[Task] {
+        &self.tasks
+    } 
+
+    fn add_task(&mut self, task: Task) {
+        self.tasks.push(task);
+    }
+}
+
+static mut TASK_MANAGER: TaskManager = TaskManager {
+    tasks: Vec::new(),
+};
 
 fn read_xml(filename: &str) -> Result<Vec<Task>, Box<dyn std::error::Error>> {
     let file = File::open(filename)?;
@@ -54,18 +77,24 @@ fn read_xml(filename: &str) -> Result<Vec<Task>, Box<dyn std::error::Error>> {
 }
 
 fn main() {
-    //Try reading tasks from sudo_task_list.xml
-    match read_xml("sudo_task_list.xml") {
-        Ok(tasks) => {
-            for task in tasks {
-                println!(
-                    "Task Desc: {}\nTask due_date: {}\nTask important: {}",
-                    task.description, task.due_date, task.important
-                );
-            }
+    let read_tasks = read_xml("sudo_task_list.xml")
+        .expect("Had trouble parsing tasks from xml file");
+
+    //Basic usage with TaskManager
+    unsafe { 
+        TASK_MANAGER.set_tasks(read_tasks); 
+        for current_task in TASK_MANAGER.fetch_tasks() {
+            println!("{:?}", current_task);
         }
-        Err(err) => {
-            eprintln!("Error: {}", err);
-        }
+
+        let new_task = Task {
+            description: String::from("new task"),
+            due_date: String::from("1/1/2024"),
+            important: String::from("y"),
+        };
+        TASK_MANAGER.add_task(new_task);
+
+        let new_task_list = TASK_MANAGER.fetch_tasks();
+        println!("Read tasks with new task added: {:?}", new_task_list);
     }
 }
